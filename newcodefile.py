dbfcc5bcde0e539b623408a64f1f776b19010eea
nataliecLOZ <natalie.chung@students.asdrp.org>
2026-07-02 17:32:24 -0700

    create wordbank list
    Updated comments for clarity and added explanation.

--- newcodefile.py
+++ newcodefile.py
@@ -1,4 +1,4 @@
-# create wordbank
+# 2. create wordbank
 wordfile = open("wordbank.txt", "r")
-wordbank = file.read().splitlines()
+wordbank = file.read().splitlines() # list of words: ['word1', 'word2', 'word3', 'etc']
 wordfile.close()
