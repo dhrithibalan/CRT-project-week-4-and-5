6f36c514768ca6694164157ef459e164cd19a84a
nataliecLOZ <natalie.chung@students.asdrp.org>
2026-07-03 12:58:14 -0700

    Finished steps 1 through 3

--- newcodefile.py
+++ newcodefile.py
@@ -1,4 +1,11 @@
+# 1. print "let's play hangman"
+print("Let's play Hangman!")
+
 # 2. create wordbank
 wordfile = open("wordbank.txt", "r")
 wordbank = file.read().splitlines() # list of words: ['word1', 'word2', 'word3', 'etc']
 wordfile.close()
+
+# 3. make dashes for num letters
+def make_dashes(word):
+  return "-"*len(word)
